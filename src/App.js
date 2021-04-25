3c93378d390bb13d6233e30399923600f34b6852
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2021-04-26 00:15:59 +0600

    Remove logo.svg
    *Assets serving doesn't work in SSR in current simple setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import logo from "./logo.svg";
-
 import "./App.css";
 
 const Section = ({ title, children }) => (
@@ -16,7 +14,6 @@ function App(props) {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
