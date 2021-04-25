9608cb81e7dcf07b70596ad537d4028ad758b712
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2021-04-25 23:13:35 +0600

    Add SSR support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+
+import "./App.css";
+
+const Section = ({ title, children }) => (
+    <section className="Section">
+        <h2>{title}</h2>
+        <button onClick={() => alert(`clicked`)}>Click me</button>
+        <div>{children}</div>
+    </section>
+);
+
+function App(props) {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Section title="Dynamic Section">
+                    {JSON.stringify(props)}
+                </Section>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ ssr/server.js
@@ -0,0 +1,57 @@
+import path from "path";
+import fs from "fs";
+
+import express from "express";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+
+import App from "../src/App";
+
+const PORT = 8080;
+const app = express();
+
+const router = express.Router();
+
+const initialProps = {
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Donec gravida tincidunt euismod. Mauris lacus mi, vulputate eget sem
+    vitae, imperdiet consectetur urna. In ante sapien, mattis eu nisi ut,
+    malesuada fringilla massa. Ut pulvinar, quam quis interdum convallis,
+    sem dolor ullamcorper lacus, vel tristique felis nunc in risus.
+    Duis ex dolor, commodo vitae metus ac, egestas tincidunt turpis.
+    Morbi leo quam, ornare molestie pellentesque interdum, euismod ut magna.
+    Aliquam tincidunt orci vulputate interdum varius.`,
+};
+
+const serverRenderer = (req, res, next) => {
+    fs.readFile(path.resolve("./build/index.html"), "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("An error occurred");
+        }
+        return res.send(
+            data.replace(
+                '<div id="root"></div>',
+                `<script>
+                    window.__initialProps = ${JSON.stringify(initialProps)}
+                </script>
+                <div id="root">${ReactDOMServer.renderToString(
+                    <App {...initialProps} />
+                )}</div>`
+            )
+        );
+    });
+};
+router.use("^/$", serverRenderer);
+
+router.use(
+    express.static(path.resolve(__dirname, "..", "build"), { maxAge: "30d" })
+);
+
+// tell the app to use the above rules
+app.use(router);
+
+// app.use(express.static('./build'))
+app.listen(PORT, () => {
+    console.log(`SSR running on port ${PORT}`);
+});
